695ed51bbf1b888e678a980ee3fe86e030a7f0b4
Guillaume Desforges <aceus02@gmail.com>
2020-05-13 11:30:06 +0200

    Use funflow from GitHub

--- app/Main.hs
+++ app/Main.hs
@@ -10,7 +10,8 @@ import Sandbox
 main :: IO ()
 main = do
     let path = [absdir|/tmp/funflow|]
-    res <- withSimpleLocalRunner path (\run -> run someTask ())
+    res <- withSimpleLocalRunner path $ \run ->
+        run someTask ()
     case res of
         Left err -> putStrLn $ "Something went wrong:" ++ show err
         Right something -> putStrLn something
